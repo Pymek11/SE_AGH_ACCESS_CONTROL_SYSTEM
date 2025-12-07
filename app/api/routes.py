797a70596ebad584be7c9e08c1d35e96a75721fe
kayen2115 <rybakm2115@gmail.com>
2025-12-07 19:48:34 +0100

    some pages i guess

--- app/api/routes.py
+++ app/api/routes.py
@@ -5,3 +5,11 @@ router = APIRouter()
 @router.get('/')
 async def read_root():
     return FileResponse('app/templates/index.html')
+
+@router.get('/login')
+async def read_login():
+    return FileResponse('app/templates/login.html')
+
+@router.get('/qr')
+async def read_qr():
+    return FileResponse('app/templates/qr.html')
